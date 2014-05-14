c530920d71548367f43e987a206892b26e2c026f
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2014-05-14 10:49:39 -0700

    Update snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -13,5 +13,13 @@ find . -name '*.jsx' | rev | cut -c 4- | rev | xargs -n 1 -I {} mv {}jsx {}js
 find . -name '*.js' -exec js-beautify -t -p -X -r -f {} \;
 
 #average of numbers in a file
+
 awk '{T+= $NF} END { print T/NR }' filename
 
+#run command n times
+
+for i in {1..5}; do cmd; done
+
+#run cmd on file w/o find
+
+for i in /some/path/*.whatev; do echo $i; done
