127ca48070a827a5aa28080b124a8effd6783a5e
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2019-07-23 11:29:27 -0400

    uniq sort group

--- snippets.sh
+++ snippets.sh
@@ -59,3 +59,45 @@ typecheck | grep 'final' | awk -F":" '{print $1}' | xargs -n 1 -I {} sed -i "s/c
 
 #split file into lines
 split -l 500 myfile segment
+
+
+unixite@sandbox:~$ cat test.txt
+one
+one
+one
+two
+two
+one
+five
+one
+one
+two
+five
+two
+one
+one
+one
+two
+two
+one
+five
+one
+one
+two
+three
+four
+two
+five
+unixite@sandbox:~$ sort test.txt  | uniq -c
+      4 five
+      1 four
+     12 one
+      1 three
+      8 two
+unixite@sandbox:~$ sort test.txt  | uniq -c | sort -n -r
+     12 one
+      8 two
+      4 five
+      1 three
+      1 four
+unixite@sandbox:~$
